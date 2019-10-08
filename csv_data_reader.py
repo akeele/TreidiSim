5acfc54bc1f93d03b8c5aaea817b1932d5c81173
akeele <aksu.suunta@gmail.com>
2019-10-08 10:32:23 +0300

    Changed 'Date' as index in the pandas dataframe

--- csv_data_reader.py
+++ csv_data_reader.py
@@ -10,10 +10,8 @@ class NasdaqOMXCsvReader:
         data = pandas.read_csv(self.csv_file, sep=';', header=1, decimal=',')
         # Drop last column, because it is empty
         data.drop(data.columns[len(data.columns)-1], axis=1, inplace=True)
+        data = data.set_index(['Date'])
         return data
 
 
 
-reader = NasdaqOMXCsvReader("/home/leiska/Desktop/ROBIT-2019-03-29-2019-04-26.csv")
-data = reader.read_to_pandas_dataframe()
-print(data.columns)
