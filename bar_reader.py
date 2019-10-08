acde0b499a7023997c68071a7238d58178b090eb
akeele <aksu.suunta@gmail.com>
2019-10-08 10:44:47 +0300

    Syntax fixes

--- bar_reader.py
+++ bar_reader.py
@@ -3,10 +3,10 @@
 class BarReader:
     
     def __init__(self, raw_dataframe, start_time, end_time):
+        self.start_time = start_time
+        self.end_time = end_time
         self.dataframe_period = self._get_dataframe_between_start_and_end(raw_dataframe)
-        self.start_date = start_date
-        self.end_date = end_date
-
+    
     def _get_dataframe_between_start_and_end(self, raw_dataframe):
         dataframe_period = raw_dataframe.loc[self.start_time:self.end_time]
         return dataframe_period
