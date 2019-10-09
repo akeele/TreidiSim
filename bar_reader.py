457ac94a375993da1dd99ed19035c38ec3e70815
akeele <aksu.suunta@gmail.com>
2019-10-09 12:11:36 +0300

    Added all_bars argument to get_bars()

--- bar_reader.py
+++ bar_reader.py
@@ -3,14 +3,18 @@
 class BarReader:
     
     def __init__(self, raw_dataframe, start_time, end_time):
+        self.raw_dataframe = raw_dataframe
         self.start_time = start_time
         self.end_time = end_time
-        self.dataframe_period = self._get_dataframe_between_start_and_end(raw_dataframe)
+        self.period_dataframe = self._get_dataframe_between_start_and_end(raw_dataframe)
     
-    def _get_dataframe_between_start_and_end(self, raw_dataframe):
-        dataframe_period = raw_dataframe.loc[self.start_time:self.end_time]
-        return dataframe_period
+    def _get_dataframe_between_start_and_end(self):
+        period_dataframe = self.raw_dataframe.loc[self.start_time:self.end_time]
+        return period_dataframe
     
-    def get_bars(self, columns)
+    def get_bars(self, columns, all_bars=False)
         #Columns are for example Opening, Close etc
-        return self.dataframe_period.columns(columns)
+        if all_bars == True:
+            return self.raw_dataframe
+        else:
+            return self.period_dataframe.columns(columns)
