0710b7186b36cea28aed97be4be1a39bb8026e68
akeele <aksu.suunta@gmail.com>
2019-10-10 10:17:11 +0300

    Scrapped bar_reader.py and added it to csv_data_reader.py

--- csv_data_reader.py
+++ csv_data_reader.py
@@ -3,15 +3,27 @@ import pandas
 
 class NasdaqOMXCsvReader:
 
+    DOHLCV_columns = ["Date", "Opening price", "High price", "Low price", "Closing price", "Total volume"]
+    DOHLCV_mapper = {"Date": "date", 
+                     "Opening price": "open",
+                     "High price": "high",
+                     "Low price": "low",
+                     "Closing price": "close",
+                     "Total volume": "volume"}
+
     def __init__(self, csv_file):
         self.csv_file = csv_file
+        self.raw_dataframe = self._read_to_pandas_dataframe()
+        self.bars = self._get_DOHLCV_bars()
+        self.bars.rename(columns=DOHLCV_mapper)
 
-    def read_to_pandas_dataframe(self):
+    def _read_to_pandas_dataframe(self):
         data = pandas.read_csv(self.csv_file, sep=';', header=1, decimal=',')
         # Drop last column, because it is empty
         data.drop(data.columns[len(data.columns)-1], axis=1, inplace=True)
         data = data.set_index(['Date'])
-        return data, data.columns
+        return data 
 
-
+    def _get_DOHLCV_bars(self):
+        return self.raw_dataframe.columns(DOHLCV_columns)
 
